1daacacc1a34a2b77c46c51ecea19bbac1d62016
Xiaolipao <143348299+Xiaolipao@users.noreply.github.com>
2023-08-28 02:01:26 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o33qQ6cBxYZOMlv0JFcoP8nok3mA',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '	BetG9_ba5JiYTdE41-MZAf6hKYf5cO8nbnAhyIgqLYk',
+      useTemplateId: 'BetG9_ba5JiYTdE41-MZAf6hKYf5cO8nbnAhyIgqLYk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-27',
       festivals: [
